d60bcbd3b261cf33eace731dc12b8b830cd194c0
Laura Do <misslaurado@gmail.com>
2017-09-18 22:01:17 -0400

    organize component props

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -3,7 +3,12 @@ import React from 'react';
 const FoodForm = (props) => {
   return(
     <form onSubmit={props.handleSubmit}>
-      <input type='text' placeholder='Add a Food' value={props.newFood} onChange={props.handleChange} />
+      <input
+        type='text'
+        placeholder='Add a Food'
+        value={props.newFood}
+        onChange={props.handleChange}
+      />
       <input type='submit' value='Add to List' />
   )
 }
